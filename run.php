7e68230fc6ff874955b6cd621362968e3a1f1fcf
Vladimir Prokhnenko <volmir@ukr.net>
2017-03-23 15:23:57 +0200

    Add code

--- run.php
+++ run.php
@@ -6,6 +6,6 @@ include 'src/BubbleSort.php';
 $binary = new BinarySearch();
 $bubble = new BubbleSort();
 
-$data = [2,3,1];
-$data = $bubble->sort($data);
-$binary->search(2, $data);
+$unsorted_data = [2,3,1];
+$sorted_data = $bubble->sort($unsorted_data);
+$result = $binary->search(2, $sorted_data);
