c57abd073e2904a9af13630727698bb2c97033e9
Vladimir Prokhnenko <volmir@ukr.net>
2017-03-24 17:02:17 +0200

    Changes in bubble sort

--- src/BubbleSort.php
+++ src/BubbleSort.php
@@ -18,14 +18,18 @@ class BubbleSort
         $iterations = $count_elements - 1;
 
         for ($i=0; $i < $count_elements; $i++) {
+            $changes = false;
             for ($j=0; $j < $iterations; $j++) {
                 if ($data[$j] > $data[($j + 1)]) {
+                    $changes = true;
                     list($data[$j], $data[($j + 1)]) = array($data[($j + 1)], $data[$j]);
                 }
             }
             $iterations--;
-        }
-        
+            if (!$changes) {
+                return $data;
+            }
+        }        
         return $data;
     }
 }
